d104f098aa13e3f024e260f174b19b308438c946
sébastien brugalières <sebastien@imac-de-sebastien-brugalieres.local>
2011-08-09 18:27:18 +0200

    Initial Commit

---
+++ ScoreBoard/ScoreBoardAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ScoreBoardAppDelegate.h
+//  ScoreBoard
+//
+//  Created by sébastien brugalières on 09/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScoreBoardAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ ScoreBoardTests/ScoreBoardTests.m
@@ -0,0 +1,32 @@
+//
+//  ScoreBoardTests.m
+//  ScoreBoardTests
+//
+//  Created by sébastien brugalières on 09/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ScoreBoardTests.h"
+
+@implementation ScoreBoardTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ScoreBoardTests");
+}
+
+@end
